c69128306388364fb6dc91c2b4c473e1112c4dc9
Joey Wilson <josephmcu2005@gmail.com>
2024-03-12 14:51:59 -0500

    removedoebat and play

--- Playground/JavaPlayground.java
+++ Playground/JavaPlayground.java
@@ -1,13 +1,7 @@
 package Playground;
 
-
-public class JavaPlayground {
+class JavaPlaygroud {
     public static void main(String[] args) {
-  
-
-        int num = 7;
-        System.out.println(num / 2);
-
-        return;
+        
     }
 }
